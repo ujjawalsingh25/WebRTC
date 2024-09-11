ba308d8a6de067a403a4cfe2bae69452fb93acd2
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-09-11 15:25:02 +0530

    Setup modules and Starter Code

---
+++ 00_Commands/GitCommands.jsx
@@ -0,0 +1,33 @@
+// __________________________  To Push Codes  ____________________________________________________
+// _____________________________________________________________________________________________|_|
+//  -->>  git init                                                                              | |
+//  -->>  git add .                                                                             | |
+//  -->>  git status                                                                            | |
+//  -->>  git commit -m "Commits"                                                               | |
+//  -->>  git branch -M main                                                                    | |
+//  -->>  git remote add origin https://github.com/ujjawalsingh25/WebRTC.git                    | |
+//  -->>  git push -u origin main                                                               | |
+// ---------------------------------------------------------------------------------------------| |
+//    git reset  -->> To revoke back to Unstaging area                                          | |
+// _____________________________________________________________________________________________|_|
+// _____________________________________________________________________________________________|_|
+
+
+// __________________________  From GitHub  ________________________________________
+//                                                                                  |
+// …or create a new repository on the command line                                  |
+//                                                                                  | 
+// echo "# WebRTC" >> README.md                                                     |
+// git init                                                                         |
+// git add README.md                                                                |
+// git commit -m "first commit"                                                     |
+// git branch -M main                                                               |
+// git remote add origin https://github.com/ujjawalsingh25/WebRTC.git               |
+// git push -u origin main                                                          |
+//                                                                                  |
+//                                                                                  |
+// …or push an existing repository from the command line                            |
+// git remote add origin https://github.com/ujjawalsingh25/WebRTC.git               |
+// git branch -M main                                                               |
+// git push -u origin main                                                          |
+// _________________________________________________________________________________|
