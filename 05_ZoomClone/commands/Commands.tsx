56b9533d4480f152688491e5f0b373e8b0568d31
Ujjawal Kumar <128883851+ujjawalsingh25@users.noreply.github.com>
2024-09-15 20:56:30 +0530

    Zoom Clone Server

---
+++ 05_ZoomClone/commands/Commands.tsx
@@ -0,0 +1,11 @@
+// Server
+    // npm init
+    // npm i nodemon
+    // npm i socket.io
+
+
+// Client
+    // npx create-react-app client   
+    // npm i react-router-dom
+    // npm i socket.io-client
+    // npm i react-player
